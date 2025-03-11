313427b6050d43659ba036c5c2d7876ac7e0467a
Aaryan657 <151989350+Aaryan657@users.noreply.github.com>
2025-03-11 11:26:42 +0530

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,13 @@
+export default {
+  API Reference: [
+    'api/api-reference',
+    'api/createstore',
+    'api/store',
+    'api/combinereducers',
+    'api/applymiddleware',
+    'api/bindactioncreators',
+    'api/compose',
+    'api/utils',
+    { type: 'link', label: 'Error Messages', href: '/errors' }
+  ],
+};
